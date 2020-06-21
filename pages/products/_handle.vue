9375fd65536410f19dfe8258543466ceafce2658
Miguel Montalvo <miguel.montalvo@barrelny.com>
2020-06-20 20:34:27 -0700

    Create simple PDP page

---
+++ pages/products/_handle.vue
@@ -0,0 +1,58 @@
+<template>
+  <div v-if="product" class="container">
+    <div>
+      <h1 class="subtitle">
+        {{ product.title }}
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import product from '~/apollo/queries/product'
+
+export default {
+  apollo: {
+    product: {
+      prefetch: ({ route }) => ({ handle: route.params.handle }),
+      query: product,
+      variables () {
+        return { handle: this.$route.params.handle }
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
