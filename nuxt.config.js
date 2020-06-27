55081b3d7d1fed1c07ea832833943cedfc3d9bd8
Miguel Montalvo <miguel.montalvo@barrelny.com>
2020-06-26 23:51:42 -0700

    Polyfill IntersectionObserver

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,9 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      { src: 'https://polyfill.io/v3/polyfill.min.js?features=IntersectionObserver', body: true }
     ]
   },
   /*
