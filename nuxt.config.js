a9343ec657e4857b936043c111f57df0b66c56c5
Miguel Montalvo <miguel.montalvo@barrelny.com>
2020-12-27 02:32:00 -0800

    :wrench: Exclude cart page

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,7 +58,7 @@ export default {
           headers: {
             'X-Shopify-Storefront-Access-Token': '0b8dcd15ac5a03fadaf2ce5fe03e331d'
           }
-        },
+        }
       }
     }
   },
@@ -82,5 +82,8 @@ export default {
         browsers: 'last 3 chrome versions, last 3 chromeandroid versions, last 3 firefox versions, last 3 opera versions, last 2 edge versions, safari >= 10, ios >= 10'
       }
     }
+  },
+  generate: {
+    exclude: [/cart/]
   }
 }
